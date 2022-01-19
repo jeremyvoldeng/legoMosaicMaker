b19c9dca63455f5c03950fb48e30038c62831a52
Axel-Jacobsen <axelnj44@gmail.com>
2022-01-18 17:47:35 -0800

    conversion to class, allowing for easier modification of variables

--- website/legoMosaicMaker.js
+++ website/legoMosaicMaker.js
@@ -36,160 +36,166 @@ legoColours = {
   "yellowish_green" : [226,249,154]
 }
 
-const FACTOR = 9;
-const SIZE = [64, 64];
 
+class Legoificator {
 
-const draw_circle = (x, y, r, colour, canvas_ctx) => {
-  let circle = new Path2D()
-  // x, y, radius, start_angle, end_angle
-  circle.arc(x, y, r, 0, 2 * Math.PI);
-  canvas_ctx.fillStyle = `rgb(${colour[0]},${colour[1]},${colour[2]})`
-  canvas_ctx.fill(circle)
-  canvas_ctx.fillStyle = 'black'
-}
-
-const init_colours_used = () => {
-  coloursUsed = {}
-  for (const colour in legoColours) {
-    coloursUsed[colour] = [0, 0]
+  constructor(factor=9, size = [64, 64]) {
+    this.factor = factor
+    this.size = size
   }
-  return coloursUsed
-}
 
-const resizeImage = (img) => {
-  const canvas = document.createElement('canvas')
-  canvas.width = SIZE[0]
-  canvas.height = SIZE[1]
-
-  // draw the image to the canvas
-  const ctx  = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0, SIZE[0], SIZE[1]);
-
-  return ctx
-}
-
-const nthOfArray = (array, period, start) => {
-  vs = []
-  for (let i = start; i < array.length; i += period) {
-    vs.push(array[i])
+  draw_circle(x, y, r, colour, canvas_ctx) {
+    let circle = new Path2D()
+    // x, y, radius, start_angle, end_angle
+    circle.arc(x, y, r, 0, 2 * Math.PI);
+    canvas_ctx.fillStyle = `rgb(${colour[0]},${colour[1]},${colour[2]})`
+    canvas_ctx.fill(circle)
+    canvas_ctx.fillStyle = 'black'
   }
-  return vs
-}
 
-const resizeImage2 = (img) => {
-  /* notes:
-   *    If this turns out to be the best method, we can integrate this
-   *    into the main processImage loop for speed.
-   *
-   *    This version is qualitatively better than resizeImage - fewer
-   *    misplaced pixels, more uniform colouring, e.t.c.
-   *
-   *    Potential improvements: gaussian-weighted averaging
-   */
-  const canvas = document.createElement('canvas')
-  canvas.width = img.width
-  canvas.height = img.height
-
-  const small_canvas = document.createElement('canvas')
-  small_canvas.width = SIZE[0]
-  small_canvas.height = SIZE[1]
-
-  const ctx  = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0, img.width, img.height);
-
-  const small_ctx  = small_canvas.getContext('2d');
-
-  const width_chunk_size = canvas.width / SIZE[0]
-  const height_chunk_size = canvas.height / SIZE[1]
-
-  for (let i = 0; i < SIZE[0]; i++) {
-    for (let j = 0; j < SIZE[1]; j++) {
-      const vals = ctx.getImageData(
-        i * width_chunk_size,
-        j * width_chunk_size,
-        width_chunk_size,
-        height_chunk_size
-      ).data
-      const rs = nthOfArray(vals, 4, 0)
-      const gs = nthOfArray(vals, 4, 1)
-      const bs = nthOfArray(vals, 4, 2)
-      const R = rs.reduce((a,b) => a + b) / rs.length
-      const G = gs.reduce((a,b) => a + b) / gs.length
-      const B = bs.reduce((a,b) => a + b) / bs.length
-      small_ctx.fillStyle = `rgb(${R}, ${G}, ${B})`
-      small_ctx.fillRect(i, j, 1, 1)
+  init_colours_used() {
+    const coloursUsed = {}
+    for (const colour in legoColours) {
+      coloursUsed[colour] = [0, 0]
     }
+    return coloursUsed
   }
 
-  return small_ctx
-}
+  resizeImage(img) {
+    const canvas = document.createElement('canvas')
+    canvas.width = this.size[0]
+    canvas.height = this.size[1]
 
-const RGBDistance = (r1, g1, b1, r2, g2, b2) => {
-  d1 = r1 - r2
-  d2 = g1 - g2
-  d3 = b1 - b2
-  return Math.sqrt(
-    d1 * d1 + d2 * d2 + d3 * d3
-  )
-}
+    // draw the image to the canvas
+    const ctx  = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, this.size[0], this.size[1]);
 
-const sRGBDistance = (r1, g1, b1, r2, g2, b2) => {
-  r = (r1 + r2) / 2
-  d1 = r1 - r2
-  d2 = g1 - g2
-  d3 = b1 - b2
-  return Math.sqrt(
-    (2 + r / 256) * d1 * d1 + 4 * d2 * d2 + (2 + (255 - r) / 256) * d3 * d3
-  )
-}
+    return ctx
+  }
 
-const getClosestLegoColour = (r, g, b, distance_metric = RGBDistance) => {
-  /* From the Colour Difference Wikipedia page[0], it turns
-   * out that distances in RGB colour space are not perceptibly
-   * uniform - that is, a colour distance of "5" will look different
-   * for different pairs of equally spaced RGB colours.
-   *
-   * This website[1] has colour conversion formulas - try Lab colour
-   * space for distances.
-   *
-   * [0] https://en.wikipedia.org/wiki/Color_difference
-   * [1] https://en.wikipedia.org/wiki/CIELAB_color_space#RGB_and_CMYK_conversions
-   * [2] stackoverflow link https://stackoverflow.com/questions/9018016/how-to-compare-two-colors-for-similarity-difference
-   */
-  minimum_colour_dist = 256
-  closest_colour = ""
-  for (let colour in legoColours) {
-    const [lego_r, lego_g, lego_b] = legoColours[colour]
-    colour_dist = distance_metric(r, g, b, lego_r, lego_g, lego_b)
-
-    if (colour_dist < minimum_colour_dist) {
-      minimum_colour_dist = colour_dist
-      closest_colour = colour
+  nthOfArray(array, period, start) {
+    const vs = []
+    for (let i = start; i < array.length; i += period) {
+      vs.push(array[i])
     }
+    return vs
   }
-  return closest_colour
-}
 
-const processImage = (input_img, input_ctx, output_ctx) => {
-  output_ctx.canvas.width = FACTOR * SIZE[0]
-  output_ctx.canvas.height = FACTOR * SIZE[1]
+  resizeImage2(img) {
+    /* notes:
+     *    If this turns out to be the best method, we can integrate this
+     *    into the main processImage loop for speed.
+     *
+     *    This version is qualitatively better than resizeImage - fewer
+     *    misplaced pixels, more uniform colouring, e.t.c.
+     *
+     *    Potential improvements: gaussian-weighted averaging
+     */
+    const canvas = document.createElement('canvas')
+    canvas.width = img.width
+    canvas.height = img.height
 
-  coloursUsed = init_colours_used()
+    const small_canvas = document.createElement('canvas')
+    small_canvas.width = this.size[0]
+    small_canvas.height = this.size[1]
 
-  const mini_input_ctx = resizeImage2(input_img)
+    const ctx  = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, img.width, img.height);
 
-  for (let i = 0; i < SIZE[0]; i++) {
-    for (let j = 0; j < SIZE[1]; j++) {
-      const [r, g, b, a] = mini_input_ctx.getImageData(i, j, 1, 1).data
-      const closest_lego_colour = getClosestLegoColour(r, g, b)
-      draw_circle(
-        i * FACTOR + FACTOR / 2,
-        j * FACTOR + FACTOR / 2,
-        FACTOR / 2,
-        legoColours[closest_lego_colour],
-        output_ctx
-      )
+    const small_ctx  = small_canvas.getContext('2d');
+
+    const width_chunk_size = canvas.width / this.size[0]
+    const height_chunk_size = canvas.height / this.size[1]
+
+    for (let i = 0; i < this.size[0]; i++) {
+      for (let j = 0; j < this.size[1]; j++) {
+        const vals = ctx.getImageData(
+          i * width_chunk_size,
+          j * width_chunk_size,
+          width_chunk_size,
+          height_chunk_size
+        ).data
+        const rs = this.nthOfArray(vals, 4, 0)
+        const gs = this.nthOfArray(vals, 4, 1)
+        const bs = this.nthOfArray(vals, 4, 2)
+        const R = rs.reduce((a,b) => a + b) / rs.length
+        const G = gs.reduce((a,b) => a + b) / gs.length
+        const B = bs.reduce((a,b) => a + b) / bs.length
+        small_ctx.fillStyle = `rgb(${R}, ${G}, ${B})`
+        small_ctx.fillRect(i, j, 1, 1)
+      }
+    }
+
+    return small_ctx
+  }
+
+  RGBDistance(r1, g1, b1, r2, g2, b2) {
+    const d1 = r1 - r2
+    const d2 = g1 - g2
+    const d3 = b1 - b2
+    return Math.sqrt(
+      d1 * d1 + d2 * d2 + d3 * d3
+    )
+  }
+
+  RGBDistance2(r1, g1, b1, r2, g2, b2) {
+    const r = (r1 + r2) / 2
+    const d1 = r1 - r2
+    const d2 = g1 - g2
+    const d3 = b1 - b2
+    return Math.sqrt(
+      (2 + r / 256) * d1 * d1 + 4 * d2 * d2 + (2 + (255 - r) / 256) * d3 * d3
+    )
+  }
+
+  getClosestLegoColour(r, g, b, distance_metric = this.RGBDistance2) {
+    /* From the Colour Difference Wikipedia page[0], it turns
+     * out that distances in RGB colour space are not perceptibly
+     * uniform - that is, a colour distance of "5" will look different
+     * for different pairs of equally spaced RGB colours.
+     *
+     * This website[1] has colour conversion formulas - try Lab colour
+     * space for distances.
+     *
+     * [0] https://en.wikipedia.org/wiki/Color_difference
+     * [1] https://en.wikipedia.org/wiki/CIELAB_color_space#RGB_and_CMYK_conversions
+     * [2] stackoverflow link https://stackoverflow.com/questions/9018016/how-to-compare-two-colors-for-similarity-difference
+     * [3] how to convert http://www.easyrgb.com/en/math.php#text2
+     */
+    let minimum_colour_dist = 256
+    let closest_colour = ""
+    for (let colour in legoColours) {
+      const [lego_r, lego_g, lego_b] = legoColours[colour]
+      const colour_dist = distance_metric(r, g, b, lego_r, lego_g, lego_b)
+
+      if (colour_dist < minimum_colour_dist) {
+        minimum_colour_dist = colour_dist
+        closest_colour = colour
+      }
+    }
+    return closest_colour
+  }
+
+  processImage(input_img, input_ctx, output_ctx) {
+    output_ctx.canvas.width = this.factor * this.size[0]
+    output_ctx.canvas.height = this.factor * this.size[1]
+
+    const coloursUsed = this.init_colours_used()
+
+    const mini_input_ctx = this.resizeImage2(input_img)
+
+    for (let i = 0; i < this.size[0]; i++) {
+      for (let j = 0; j < this.size[1]; j++) {
+        const [r, g, b, a] = mini_input_ctx.getImageData(i, j, 1, 1).data
+        const closest_lego_colour = this.getClosestLegoColour(r, g, b)
+        this.draw_circle(
+          i * this.factor + this.factor / 2,
+          j * this.factor + this.factor / 2,
+          this.factor / 2,
+          legoColours[closest_lego_colour],
+          output_ctx
+        )
+      }
     }
   }
 }
